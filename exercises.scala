94b123dbae2e35fe780b6af8e3c639f2103f4933
Matt DeBoard <matt.deboard@gmail.com>
2013-07-10 01:46:58 -0400

    Finish up chapter 2.

--- exercises.scala
+++ exercises.scala
@@ -1,4 +1,6 @@
 import scala.collection.immutable.IndexedSeq
+import scala.math.pow
+
 // 2.1
 def getSignum(n: Int) : Int = {
   if (n > 0) 1 else if (n < 0) -1 else 0
@@ -32,18 +34,18 @@ def countdown(n: Int) {
 }
 
 // 2.6
-def unicodeProduct(s: String) : Long = {
-  var start : Long = 1L
+def unicodeProduct(s: String) : Int = {
+  var start : Int = 1
   for (i <- s) {
-    start *= i.toLong
+    start *= i.toByte
   }
   start
 }
 
 // 2.7
-def unicodeProductNoLoop(s: String) : Long = {
-  val s2 : IndexedSeq[Long] = s map (c => c.toLong)
-  s2.foldLeft(1L) { (total, n) =>
+def unicodeProductNoLoop(s: String) : Int = {
+  val s2 : IndexedSeq[Byte] = s map (c => c.toByte)
+  s2.foldLeft(1) { (total, n) =>
     total * n
   }
 }
@@ -52,9 +54,13 @@ def unicodeProductNoLoop(s: String) : Long = {
 // Covered above
 
 // 2.9
-def recurProduct(s: String, start: Long = 1L) : Long = {
-  if (s.length == 1) return s.head.toLong * start
-  recurProduct(s.tail, s.head.toInt * start)
+def recurProduct(s: String, start: Int = 1) : Int = {
+  if (s.length == 1) return s.head.toByte * start
+  recurProduct(s.tail, s.head.toByte * start)
 }
 
 // 2.10
+// This one is kind of dumb, just turns out to be silly algebra gotcha.
+def xToThe(x: Double, n: Int) : Double = {
+  pow(x, n)
+}
